b5e52bf41676242b3764873ead05285a824ac38c
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-10-30 09:31:05 -0300

    Add files via upload

--- ricos.py
+++ ricos.py
@@ -10,4 +10,18 @@ def carrega_dados():
 def titulo(texto, traco="-"):
     print()
     print(texto)
+    print(traco*40)
+
+
+# ---------------------------------------------------------------------  Programa Principal
+carrega_dados()
+
+while True:
+    titulo("Forbes: Bilionários 2023", "=")
+    print("1. Top 20 Bilionários")
+    print("2. Comparativo entre 2 Países")
+    print("3. Agrupar por Atividade")
+    print("4. Gráfico Relacionando Atividades")
+    print("5. Finalizar")
+    opcao = int(input("Opção: "))
 
