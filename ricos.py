be7f92c598d070320abc5d53bb3919945b7cb2f9
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-10-30 09:44:05 -0300

    Add files via upload

--- ricos.py
+++ ricos.py
@@ -12,6 +12,18 @@ def titulo(texto, traco="-"):
     print(texto)
     print(traco*40)
 
+def top_20():
+    titulo("Top 20: Bilionários")
+    
+    print("\nNº Nome........................ Pais........................ Atividade........................ Bills")
+    
+    contador = 0 
+
+    for rico in ricos:
+        contador += 1
+
+        print(f"{contador:2d} {rico['name']:20} {rico['country']:20} {rico['industry']:20} {rico['net_worth']:>5}")
+        
 
 # ---------------------------------------------------------------------  Programa Principal
 carrega_dados()
@@ -24,4 +36,15 @@ while True:
     print("4. Gráfico Relacionando Atividades")
     print("5. Finalizar")
     opcao = int(input("Opção: "))
+    if opcao == 1:
+        top_2()
+    elif opcao == 2:
+        compara_paises()
+    elif opcao == 3:
+        agrupar_atividade()
+    elif opcao == 4:
+        grafico_atividades()
+    else:
+        break
+
 
