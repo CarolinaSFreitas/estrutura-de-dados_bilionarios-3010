5c913a6cfbd97a49fbe22ddf270514e4d920de7a
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-10-30 09:27:07 -0300

    Add files via upload

---
+++ ricos.py
@@ -0,0 +1,13 @@
+import csv
+ricos = []
+
+def carrega_dados():
+    with open('forbes_2640_billionaires.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for linha in csv_reader:
+            ricos.append(linha)    # lista de dicionários
+
+def titulo(texto, traco="-"):
+    print()
+    print(texto)
+
